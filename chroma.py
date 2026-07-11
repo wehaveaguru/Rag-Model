c7ef28ef66ca39d119c3fa6ed9b3f94791817961
Guru <207536326+wehaveaguru@users.noreply.github.com>
2026-07-11 20:28:10 +0530

    Added embedding functions

--- chroma.py
+++ chroma.py
@@ -1,16 +1,27 @@
 import chromadb
-
+from main import embeddings
 
 chroma_client = chromadb.Client()
 collection = chroma_client.create_collection(name="my_collection")
 
 collection.add(
     ids= ["id1","id2"],
-    documents= ["This is a document about pineapples","This is a document about apples"]
+    documents= ["This is a document about pineapples",
+                "This is a document about apples"],
+    embeddings=embeddings,
+    metadatas=[
+        {
+            "subject":"pineapple"
+        },
+        {
+            "subject" : "apple"
+        }
+    ]
 )
 
 results=collection.query(
     query_texts=["This document is about pomegrenates"],
+    
     n_results=2
 )
 
