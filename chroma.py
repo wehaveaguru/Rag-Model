764f98b56bde4dd04061303cbb2db1101c9fe9bb
Guru <207536326+wehaveaguru@users.noreply.github.com>
2026-07-11 18:34:07 +0530

    created chroma database

---
+++ chroma.py
@@ -0,0 +1,17 @@
+import chromadb
+
+
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="my_collection")
+
+collection.add(
+    ids= ["id1","id2"],
+    documents= ["This is a document about pineapples","This is a document about apples"]
+)
+
+results=collection.query(
+    query_texts=["This document is about pomegrenates"],
+    n_results=2
+)
+
+print(results)
